1ca75d423bae1dab9a4c9fa628fcd3ab8267e720
blackle <isabelle@blackle-mori.com>
2024-06-06 19:34:03 -0400

    madvise and setcap

--- libmiku.c
+++ libmiku.c
@@ -5,9 +5,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <dlfcn.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <errno.h>
+
 #include "miku_title.h"
 #include "miku_data.h"
-#include <time.h>
+
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_surface.h>
 
@@ -15,6 +19,18 @@ typedef FILE* (*fopen_fun_ptr)(const char *, const char *);
 
 __attribute__((constructor)) void seed_rng() {
 	srand(getpid() + time(NULL));
+}
+
+// advise that we're gonna be randomly accessing our miku dataset
+// this is overkill, but it is fun overkill :3
+__attribute__((constructor)) void advise_random() {
+	// 0x7fffe56580ad-0x7fffe5658000
+	void* start = (void*)&_binary_miku_data_bin_start - (((long long int)&_binary_miku_data_bin_start) & 0x7ff); //round down to page offset(hopefully)
+	void* end = (void*)&_binary_miku_data_bin_end;
+	int res = madvise(start, (size_t)(end - start), MADV_RANDOM);
+	if (res != 0) {
+		printf("couldn't madvise - %s\n", strerror(errno));
+	}
 }
 
 FILE *fopen(const char *pathname, const char *mode) {
@@ -54,4 +70,11 @@ int SDL_UpperBlit(SDL_Surface * src, const SDL_Rect * srcrect, SDL_Surface * dst
 		return orig_blit(last_src_replacement, srcrect, dst, dstrect);
 	}
 	return orig_blit(src, srcrect, dst, dstrect);
+}
+
+typedef int (*setcap_ptr)(const char*, const char*);
+
+void SDL_WM_SetCaption(const char *title, const char *icon) {
+	setcap_ptr original_setcap = (setcap_ptr)dlsym(RTLD_NEXT, "SDL_WM_SetCaption");
+	original_setcap("Texmaster 2009: Hatsune Miku Edition", icon);
 }
